c51cffabb2bf4768ceb6c86f3d64da64889fdac4
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2017-02-27 12:22:47 +0100

    Initial version

---
+++ src/test/js/MarkdownEditorSpec.js
@@ -0,0 +1,15 @@
+
+
+(function () {
+
+    "use strict";
+
+    describe("Test MarkdownEditor", function () {
+
+        it("Dummy test", function () {
+            expect(true).toBeTruthy();
+        });
+
+    });
+
+})();
